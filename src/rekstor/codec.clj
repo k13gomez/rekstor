7644c30b9b30618347352527fe2882c76cca09de
Jose Gomez <k13gomez@gmail.com>
2019-04-29 22:58:10 -0400

    implement codec person decoder

--- src/rekstor/codec.clj
+++ src/rekstor/codec.clj
@@ -1,8 +1,24 @@
 (ns rekstor.codec
   (:require [rekstor.spec :as spec]
-            [clojure.spec.alpha :as s])
+            [rekstor.data :refer [->Person]]
+            [clojure.spec.alpha :as s]
+            [clojure.string :as string])
   (:import [java.time LocalDate]
            [java.time.format DateTimeParseException]))
+
+(def pipe-delimited-format
+  :pipe-delimited)
+
+(def comma-delimited-format
+  :comma-delimited)
+
+(def space-delimited-format
+  :space-delimited)
+
+(def format-to-pattern
+  {pipe-delimited-format #"\|"
+   comma-delimited-format #","
+   space-delimited-format #" "})
 
 (defn to-local-date
   "parses a local date in the format yyyy-MM-dd or
@@ -13,3 +29,18 @@
       (LocalDate/parse str)
       (catch DateTimeParseException _
         nil))))
+
+(defn to-person
+  "creates a Person record from last-name, first-name, gender, favorite-color and date-of-birth"
+  [& [last-name first-name gender favorite-color date-of-birth]]
+  (->> (to-local-date date-of-birth)
+    (->Person last-name first-name gender favorite-color)))
+
+(defn decode-person
+  "converts a line of text by splitting the fields using the split pattern,
+  then converts it to a Person record"
+  [^String text format]
+  (->> (format-to-pattern format)
+    (string/split text)
+    (map string/trim)
+    (apply to-person)))
--- test/rekstor/codec_test.clj
+++ test/rekstor/codec_test.clj
@@ -1,6 +1,7 @@
 (ns rekstor.codec-test
   (:require [clojure.test :refer :all]
-            [rekstor.codec :refer :all])
+            [rekstor.codec :refer :all]
+            [rekstor.data :refer [->Person]])
   (:import [java.time LocalDate]))
 
 (deftest to-local-date-test
@@ -8,3 +9,18 @@
     (is (= (to-local-date "2018-01-01") (LocalDate/parse "2018-01-01")))
     (is (nil? (to-local-date nil)))
     (is (nil? (to-local-date "Jan 1st, 2018")))))
+
+(deftest to-person-test
+  (testing "validate to-person"
+    (let [dob (LocalDate/parse "2018-01-01")
+          expected (->Person "Smith" "John" "M" "Blue" dob)]
+      (is (= (to-person "Smith" "John" "M" "Blue" "2018-01-01") expected))
+      (is (not= (to-person) expected)))))
+
+(deftest decode-person-test
+  (testing "validate decode person record"
+    (let [expected (->Person "Smith" "John" "M" "Blue" (LocalDate/parse "2018-01-01"))]
+      (is (= (decode-person "Smith, John, M, Blue, 2018-01-01" comma-delimited-format) expected))
+      (is (= (decode-person "Smith | John | M | Blue | 2018-01-01" pipe-delimited-format) expected))
+      (is (= (decode-person "Smith John M Blue 2018-01-01" space-delimited-format) expected))
+      (is (not= (decode-person "Smith, John, M, Blue" comma-delimited-format) expected)))))
